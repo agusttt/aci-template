6ff61bb1c5870e68f0c6440f1b5911c66adea249
agusttt <agussusilo0893@gmail.com>
2024-12-22 15:59:04 +0700

    updates add page contact

--- contact-us.php
+++ contact-us.php
@@ -10,6 +10,7 @@
 	<link rel="stylesheet" href="assets/css/reset.css">
 	<link rel="stylesheet" href="assets/css/mobile-style.css">
 	<link rel="stylesheet" href="assets/css/hamburgers.min.css">
+	<link rel="stylesheet" href="assets/css/select2.min.css">
 	<link rel="stylesheet" href="assets/css/style.css">
 </head>
 
@@ -192,112 +193,151 @@
 						</nav>
 					</div>
 				</div>		
+
 				<div class="row">
-					<div class="col"></div>
-					<div class="col-lg-10 col-md-10 col-sm-12 col-12 column-form-contact">
-						<div class="wrp-form-contact">
+				<div class="col-xl-7 col-lg-7 col-md-12 col-sm-12 col-contact-us order-xl-1 order-lg-1 order-md-12 order-sm-12 order-xs-12 order-12">
+					<div class="left-text-contact">
+						<h3>Contact Us for Questions or Support</h3>
+					</div>
+					<div class="row mt-5 mx-n5">
+						<div class="col-md-6 col-sm-12 col-12 px-5">
+							<div class="connect-with-us-contact">
+							<h6>Head Office Address</h6>
+								<p>Menteng, Central Jakarta, Indonesia</p>							
+							</div>										
+						</div>
+						<div class="col-md-6 col-sm-12 col-12 px-5">
+							<div class="donor-services-contact">
+								<a href="tel:+6221509999" class="icon-text-contact">
+									<div class="icon-30">
+										<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+											viewBox="0 0 76 76" style="enable-background:new 0 0 76 76;" xml:space="preserve">
+											<g>
+												<path class="st0" d="M45.5,74.1c-7.6,0-18.4-2.4-29.2-13.2C4.9,49.4,2.9,37.9,3.2,30.3c0.2-4.1,2.2-8.1,5.9-11.9
+													c2.9-2.9,7-4.3,10.9-3.8c3.6,0.5,6.6,2.5,8.3,5.6l2.2,4c2.5,4.5,1.6,10.4-2.4,14.4c-0.7,0.8-0.9,3.3,3.1,7.3
+													c3.9,3.9,6.5,3.8,7.3,3.1c3.9-3.9,9.8-4.8,14.3-2.3l4,2.2c3.1,1.7,5.1,4.7,5.6,8.3c0.5,3.9-0.9,8-3.8,10.9C55,71.9,51,73.9,46.9,74
+													C46.5,74,46,74.1,45.5,74.1z M18.3,19.4c-2.1,0-4.2,0.9-5.8,2.5c-2,2-4.4,5.1-4.5,8.6c-0.3,6.6,1.6,16.7,11.8,26.9
+													S40,69.4,46.7,69.2c3.5-0.1,6.6-2.5,8.6-4.5c1.8-1.8,2.8-4.4,2.4-6.8c-0.3-2.1-1.4-3.7-3.2-4.7l-4-2.2c-2.6-1.5-6.1-0.8-8.5,1.5
+													l0,0c-0.8,0.8-2.1,1.4-3.5,1.7c-2.3,0.4-6.1-0.1-10.7-4.8c-8.6-8.6-3.3-14-3.1-14.2c2.4-2.4,3-5.9,1.5-8.5l-2.2-4
+													c-1-1.8-2.7-2.9-4.7-3.2C19,19.4,18.7,19.4,18.3,19.4z"/>
+												<path class="st0" d="M70.4,32c-1.2,0-2.2-0.9-2.4-2.1c-1.3-9.2-7.6-15.2-7.6-15.2c-6-6-13.6-7.9-13.7-7.9c-1.3-0.3-2.1-1.6-1.8-3
+													c0.3-1.3,1.6-2.1,2.9-1.8c0.4,0.1,9,2.3,15.9,9.1c0.3,0.2,7.5,7.1,9.1,18.1c0.2,1.3-0.7,2.6-2.1,2.8C70.7,32,70.5,32,70.4,32z"/>
+												<path class="st0" d="M57,31.7c-0.9,0-1.8-0.6-2-1.6c-2.4-9.2-10.8-11-11.2-11.1c-1.1-0.2-1.9-1.3-1.7-2.5c0.2-1.1,1.3-1.9,2.4-1.7
+													C45,15,55.9,17.2,59,29c0.3,1.1-0.4,2.3-1.5,2.6C57.3,31.6,57.1,31.7,57,31.7z"/>
+											</g>
+										</svg>
+									</div>
+									<div class="label-text-phone">
+											<h6>Telephone</h6>
+											<p>021-509999</p>
+									</div>
+								</a>
+								<a href="mailto:mail.service@aci.id" class="icon-text-contact">
+									<div class="icon-30">
+										<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+											viewBox="0 0 76 76" style="enable-background:new 0 0 76 76;" xml:space="preserve">
+											<g>
+												<path class="st0" d="M37.6,40.8c-1.6,0-3.3-0.5-4.5-1.6l0,0l-20.4-18c-1.7-1.5-2.3-3.7-1.5-5.7c0.9-2.3,3.2-3.8,6-3.8H57
+													c2.8,0,5.2,1.5,6,3.8c0.8,2,0.2,4.2-1.5,5.7L42.2,39.1C40.9,40.2,39.3,40.8,37.6,40.8z M36.2,35.7c0.7,0.6,2.2,0.6,2.9,0l19.3-17.9
+													c0.4-0.3,0.3-0.5,0.3-0.6c-0.1-0.3-0.7-0.8-1.7-0.8H17.2c-1,0-1.6,0.5-1.7,0.8c0,0.1-0.1,0.2,0.2,0.5L36.2,35.7z"/>
+												<path class="st0" d="M61.4,68.8H13.7c-6,0-10.9-4.9-10.9-10.9V27.6c0-1.6,0.9-3,2.4-3.7c1.5-0.7,3.1-0.4,4.3,0.6l26,22.7
+													c1.1,1,2.8,1,4,0l26-22.7c1.2-1.1,2.9-1.3,4.3-0.6c1.5,0.7,2.4,2.1,2.4,3.7v30.3C72.3,63.9,67.4,68.8,61.4,68.8z M8.2,30.6v27.3
+													c0,3,2.5,5.5,5.5,5.5h47.7c3,0,5.5-2.5,5.5-5.5V30.6L43.1,51.3c-3.2,2.8-7.9,2.8-11.1,0L8.2,30.6z M69.1,28.6L69.1,28.6
+													C69.1,28.6,69.1,28.6,69.1,28.6z M67.4,26.6L67.4,26.6L67.4,26.6z"/>
+											</g>
+										</svg>
+									</div>
+									<div class="label-text-phone">
+											<h6>Email</h6>
+											<p>mail.service@aci.id</p>
+									</div>
+								</a>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-11 col-sm-12 col-12">
+							<div class="wrp-maps" style="-webkit-filter: grayscale(86%);filter: grayscale(86%);">
+							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4855.2180563572565!2d106.8260358235198!3d-6.196514769226873!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f426b54f9b3d%3A0xcf7a2816bef6181b!2sGedung%20Menteng%20Central!5e0!3m2!1sen!2sid!4v1734850600495!5m2!1sen!2sid" width="600" height="320" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></div>						</div>
+					</div>
+				</div>
+				<div class="col-xl-5 col-lg-5 col-md-12 col-sm-12 order-xl-12 order-lg-12 order-md-1 order-sm-1 order-xs-1 order-1 col-contact-us">
+					<div class="wrp-form">
+						<div class="title-say-hi">
+							<h5>Write message</h5>
+						</div>
+						<form class="form-contact-us needs-validation" method="post" action="https://yappika-actionaid.or.id/id/tentang-kami/contact" novalidate="">
 							<div class="row">
 								<div class="col-12">
-									<div class="wrp-title-form-contact">
-										<h1>Write <span>Your Message</span></h1>
-									</div>
-								</div>
+									<div class="form-group form-label-group form-group-line">
+										<input type="email" id="inputEmail" class="form-control reset-box-shadow " name="contact_email" value="" placeholder="" autofocus="" required="">
+																				<!-- <div class="invalid-feedback">Email adress is required</div>  --> 
+										<label for="contact_email">Email Adress</label>
+									</div>  									        
+								</div>								
 							</div>
 							<div class="row">
-								<div class="col-lg-6 col-md-6 col-sm-12 col-12">
+								<div class="col-md-6 col-sm-12 col-xs-12 col-12">
 									<div class="form-group form-label-group form-group-line">
-										<input type="email" id="inputEmail" class="form-control reset-box-shadow" placeholder="" autofocus="" required="">
-										<div class="invalid-feedback">Email adress is required</div>  
-										<label for="inputEmail">Email Adress</label>
-									</div>								
+										<input type="text" id="inputName" class="form-control reset-box-shadow " name="contact_name" value="" placeholder="" required="">
+																				<!-- <div class="invalid-feedback">Name label is required</div>  --> 
+										<label for="contact_name">Full Name</label>
+									</div>   
 								</div>
-								<div class="col-lg-6 col-md-6 col-sm-12 col-12">
+								<div class="col-md-6 col-sm-12 col-xs-12 col-12">
 									<div class="form-group form-label-group form-group-line">
-										<input type="text" id="inputName" class="form-control reset-box-shadow" placeholder="" required="">
-										<div class="invalid-feedback">Name label is required</div>  
-										<label for="inputName">Full Name</label>
-									</div>								
-								</div>
-								<div class="col-lg-6 col-md-6 col-sm-12 col-12">
+										<input type="text" id="inputPhone" class="form-control reset-box-shadow " name="contact_phone" value="" placeholder="" required="">
+																				<!-- <div class="invalid-feedback">Phone number is required</div>  --> 
+										<label for="contact_phone">Phone Number</label>
+									</div>   
+								</div>								
+							</div>
+							<div class="row">
+								<div class="col-md-12 col-sm-12 col-xs-12 col-12">
 									<div class="form-group form-label-group form-group-line">
-										<input type="text" id="inputPhone" class="form-control reset-box-shadow" placeholder="" required="">
-										<div class="invalid-feedback">Phone number is required</div>  
-										<label for="inputPhone">Phone Number</label>
-									</div>								
+										<input type="text" id="inputName" class="form-control reset-box-shadow " name="contact_company" value="" placeholder="" required="">
+																				<!-- <div class="invalid-feedback">Company / Organization is required</div>   -->
+										<label for="contact_company">Company / Organization</label>
+									</div>   
 								</div>
-								<div class="col-lg-6 col-md-6 col-sm-12 col-12">
-									<div class="form-group form-label-group form-group-line">
-										<input type="text" id="inputName" class="form-control reset-box-shadow" placeholder="" required="">
-										<div class="invalid-feedback">Company / Organization is required</div>  
-										<label for="inputName">Company / Organization</label>
-									</div>								
+							</div>	
+							<div class="row">
+								<div class="col-12">
+									<div class="form-group form-group-line form-group-select">
+									<select class="form-control custom-select select-subject " name="contact_subject" required="">
+										<!-- <option value="0"></option> --> 
+										<option value="0">Select subject</option>
+																					<option value="7"> Donor Services</option>
+																					<option value="8"> Office</option>
+																				<!-- <option value="2">Select subject 1</option>
+										<option value="3">Select subject 3</option>
+										<option value="4">Select subject 4</option> -->
+									</select>
+									<div class="invalid-feedback">Please select subject</div>
+									</div> 									
 								</div>
-								<div class="col-lg-6 col-md-6 col-sm-12 col-12">
+							</div>	
+							<div class="row">
+								<div class="col-12">
 									<div class="form-group form-group-line form-group-text">
-										<textarea rows="4" class="form-control textarea-contact form-label-static reset-box-shadow" id="validationTextarea" placeholder="Your message" required=""></textarea>
-										<div class="invalid-feedback">
+										<textarea name="contact_message" rows="4" class="form-control textarea-contact form-label-static reset-box-shadow " id="validationTextarea" placeholder="Your message" required=""></textarea>
+																				<div class="invalid-feedback">
 										Please enter a message in the textarea.
 										</div>             
-									</div>								
+									</div>              
+								</div>								
+							</div>		
+							<div class="row">
+								<div class="col-12">
+									<button type="submit" class="btn btn-contact-us">Submit</button>
 								</div>
-								<div class="col-lg-6 col-md-6 col-sm-12 col-12">
-
-								</div>
-								<div class="col-lg-12 col-md-12 col-sm-12 col-12">
-									<button type="submit" class="btn d-block btn-contact-us">Send Message</button>
-								</div>
-							</div>
-						</div>
+							</div>				
+						</form>
 					</div>
-					<div class="col"></div>
-				</div>	
+				</div>
+			</div>
 			</form>
 		</div>
-	</section>
-
-	<section class="section-maps-head-office">
-		<div class="row align-items-center custom-align-maps">
-			<div class="col-lg-6 col-md-6 col-sm-12 col-12 col-maps-head-office">
-				<div class="wrp-maps-head-office">
-					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.7701412490787!2d106.7831886153412!3d-6.293910295444733!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f1da4ab3bd11%3A0xd048f304a53e2661!2sCitibank%20Tower%20B%20-%20South%20Quarter!5e0!3m2!1sen!2sid!4v1622713257258!5m2!1sen!2sid" width="100%" height="362" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-				</div>
-			</div>
-			<div class="col-lg-6 col-md-6 col-sm-12 col-12 col-address-head-office">
-				<div class="wrp-address-head-office">
-					<div class="text-address-head-office">
-						<h2>Head Office</h2>
-						<p>Jl. R.A.Kartini No.Kav. 8, RT.10/RW.4, Cilandak Bar., Jakarta, Kota Jakarta Selatan, Jakarta 12430, Indonesia</p>
-						<p>Phone: +62 21 227-22172, 222-22173</p>
-						<div class="img-icon">
-							<img width="56" src="assets/images/icon-helm-contruction.png" alt="helm">
-						</div>						
-					</div>
-				</div>				
-			</div>			
-		</div>
-	</section>
-	<section class="section-maps-fam">
-		<div class="row align-items-center custom-align-maps">
-			<div class="col-lg-6 col-md-6 col-sm-12 col-12 col-address-head-office">
-				<div class="col-address-head-office-flexy  justify-content-end">
-					<div class="wrp-address-head-office">
-						<div class="text-address-head-fam">
-							<h2>Head Office</h2>
-							<p>Jl. R.A.Kartini No.Kav. 8, RT.10/RW.4, Cilandak Bar., Jakarta, Kota Jakarta Selatan, Jakarta 12430, Indonesia</p>
-							<p>Phone: +62 21 227-22172, 222-22173</p>
-							<div class="img-icon-right">
-								<img width="56" src="assets/images/icon-helm-contruction.png" alt="helm">
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col-lg-6 col-md-6 col-sm-12 col-12 col-address-head-office">
-				<div class="wrp-maps-head-office">
-					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.7701412490787!2d106.7831886153412!3d-6.293910295444733!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f1da4ab3bd11%3A0xd048f304a53e2661!2sCitibank%20Tower%20B%20-%20South%20Quarter!5e0!3m2!1sen!2sid!4v1622713257258!5m2!1sen!2sid" width="100%" height="362" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-				</div>
-			</div>	
-		</div>		
 	</section>
 
 	<!-- section image before footer -->
@@ -406,11 +446,15 @@
 	<script src="assets/js/simpleParallax.min.js"></script>
 	<script src="assets/js/jquery.menu-aim.js"></script>
 	<script src="assets/js/main.js"></script>	
+	<script src="assets/js/select2.min.js"></script>	
 	<script src="assets/js/custom.js"></script>	
 	<script>
 		var image = document.getElementsByClassName('footerpresiden');
 			new simpleParallax(image, {
 			orientation: 'right'
+		});
+		$('.select-subject').select2({
+			placeholder: "Choose Subject",  
 		});
 	</script>	
 </body>
